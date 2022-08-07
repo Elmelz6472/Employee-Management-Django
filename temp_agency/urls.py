4c8800f70a0d8d94beaa9de60bdcd6160ae87481
Malik Macbook <malikmacbook@MalikMacbook.local>
2022-08-06 20:52:43 -0400

    fixed main url redirect

--- temp_agency/urls.py
+++ temp_agency/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('employee_management/', include('employee_management.urls')),
-    path("contract_management/", include('contract_management.urls'))
+    path("contract_management/", include('contract_management.urls')),
+    path("", RedirectView.as_view(url='/employee_management/'), name='login-redirect')
 ]
