e43fadddfbd99b719d7fafe51a175d8284d074ed
Malik Macbook <malikmacbook@MalikMacbook.local>
2022-09-10 18:06:14 -0400

    hided secret key

--- temp_agency/settings.py
+++ temp_agency/settings.py
@@ -1,6 +1,8 @@
 import dj_database_url
 from pathlib import Path
 import os
+import dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,8 +11,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
+
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
+
+
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-8=w#f25((d&o09=oj4-dte5ntw_el5s^*+yi5308b29#4axank"
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
